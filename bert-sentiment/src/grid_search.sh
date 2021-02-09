6be76e981bf2e78666e0125aac28e1d0420c595e
Matīss Rikters <matiss_rikters@hotmail.com>
2021-02-09 10:32:51 +0900

    Updated gridsearch script

--- bert-sentiment/src/grid_search.sh
+++ bert-sentiment/src/grid_search.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 
-for lrate in 0.0001 0.00005 0.00001 0.000005
+for lrate in 0.00005 0.00001 0.000005
 do
 	for drop in 0.2 0.3 0.4 0.5
 	do
-		echo "Learning rate"
-		echo $lrate
-		echo "Dropout"
-		echo $drop
+		echo "Learning rate - ${lrate}"
+		echo "Dropout - ${drop}"
 		python train.py --lr=$lrate --dropout=$drop
 	done
 done
